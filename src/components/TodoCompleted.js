37eb1e1febc633c4fd056d4bb41108691e394248
victor-Casta <victorarroyavecastaneda@gmail.com>
2024-03-02 18:54:38 -0500

    🚀  add: creación de grafico
    grafico para el componente TodoCompleted

--- src/components/TodoCompleted.js
+++ src/components/TodoCompleted.js
@@ -1,6 +1,11 @@
+import '../css/TodoCompleted.css'
+
 function TodoCompleted() {
   return (
-    <h2>Tareas Completadas</h2>
+    <div className="TodoCompleted-container">
+      <h2>Tareas Completadas</h2>
+      <div className="task-graphic"></div>
+    </div>
   )
 }
 
