3e88b4093a28b94f3778aeafb7bbfa1a3fb66b66
Victor Casta <victorarroyavecastaneda@gmail.com>
2024-03-05 13:08:02 -0500

    add:  nuevo estilo de grafico
    instalación de libreria y creación del nuevo diseño

--- src/components/TodoCompleted.js
+++ src/components/TodoCompleted.js
@@ -1,12 +1,28 @@
-import '../css/TodoCompleted.css'
+import ProgressBar from "react-customizable-progressbar";
+import "../css/TodoCompleted.css";
 
 function TodoCompleted() {
   return (
     <div className="TodoCompleted-container">
       <h2>Tareas Completadas</h2>
-      <div className="task-graphic"></div>
+      <div className="graphic-container">
+        <ProgressBar
+          progress={50}
+          strokeColor={"#85e486"}
+          trackStrokeWidth={10}
+          strokeWidth={10}
+          trackStrokeColor={"#292929"}
+          pointerRadius={10}
+          pointerStrokeWidth={5}
+          pointerStrokeColor={"#85e486"}
+          initialAnimation={true}
+          initialAnimationDelay={2000}
+          className={"graphic"}
+        />
+        <p>50%</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export { TodoCompleted };
