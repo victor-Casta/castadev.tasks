182ee6edc26eb8dfe91ab2f8cd867563af7ae487
Victor Casta <victorarroyavecastaneda@gmail.com>
2024-03-05 14:18:27 -0500

    add: gráfico con datos dinámicos

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -26,77 +26,77 @@ import { TodoContext } from "../TodoContext/context";
 function AppUI() {
   return (
     <div className="App">
-      <section className="principal-content">
-        <aside>
-          <div className="profile-content">
-            <img src={imageProfile} alt="Profile" />
-            <div>
-              <h2>castaDev</h2>
-              <p>WorkSpace</p>
-            </div>
-          </div>
-          <div className="items-list">
-            <ul>
-              <ItemsAside icon={taskIcon} title={"Tasks"} />
-            </ul>
-          </div>
-        </aside>
-        <main>
-        <TodoContext.Consumer>
-          {({
-            loading,
-            error,
-            completedTodos,
-            totalTodos,
-            searchValue,
-            setSearchValue,
-            searchedTodos,
-            completeTodo,
-            deleteTodo,
-          }) => (
-            <>
-            <header>
-              <TodoSearch
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-              />
-            </header>
-            <section className="tasks-list__container">
-              <div className="header__content">
-                <h2>Tasks</h2>
-              </div>
-              <div className="items__container">
-                <TodoCounter completed={completedTodos} total={totalTodos} />
-                {loading && <TodoLoading />}
-                <ul>
-                  {error && <TodosError />}
-                  {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
-                  {searchedTodos.map((todo) => (
-                    <TodoItems
-                      key={todo.text}
-                      text={todo.text}
-                      icon={taskListItem}
-                      completed={todo.completed}
-                      onComplete={() => completeTodo(todo.text)}
-                      onDelete={() => deleteTodo(todo.text)}
-                    />
-                  ))}
-                </ul>
-              </div>
+      <TodoContext.Consumer>
+        {({
+          loading,
+          error,
+          completedTodos,
+          totalTodos,
+          searchValue,
+          setSearchValue,
+          searchedTodos,
+          completeTodo,
+          deleteTodo,
+        }) => (
+          <>
+            <section className="principal-content">
+              <aside>
+                <div className="profile-content">
+                  <img src={imageProfile} alt="Profile" />
+                  <div>
+                    <h2>castaDev</h2>
+                    <p>WorkSpace</p>
+                  </div>
+                </div>
+                <div className="items-list">
+                  <ul>
+                    <ItemsAside icon={taskIcon} title={"Tasks"} />
+                  </ul>
+                </div>
+              </aside>
+              <main>
+                <header>
+                  <TodoSearch
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                  />
+                </header>
+                <section className="tasks-list__container">
+                  <div className="header__content">
+                    <h2>Tasks</h2>
+                  </div>
+                  <div className="items__container">
+                    <TodoCounter completed={completedTodos} total={totalTodos} />
+                    {loading && <TodoLoading />}
+                    <ul>
+                      {error && <TodosError />}
+                      {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
+                      {searchedTodos.map((todo) => (
+                        <TodoItems
+                          key={todo.text}
+                          text={todo.text}
+                          icon={taskListItem}
+                          completed={todo.completed}
+                          onComplete={() => completeTodo(todo.text)}
+                          onDelete={() => deleteTodo(todo.text)}
+                        />
+                      ))}
+                    </ul>
+                  </div>
+                </section>
+              </main>
+            </section>
+            <section className="secondary-content">
+              <section className="create-todo__container">
+                <CreateTodo />
+              </section>
+              <section className="todo-completed">
+                <TodoCompleted total={totalTodos} completed={completedTodos} />
+              </section>
             </section>
           </>
-          )}
-        </TodoContext.Consumer>
-        </main>
-      </section>
-      <section className="secondary-content">
-        <section className="create-todo__container">
-          <CreateTodo />
-        </section>
-        <section className="todo-completed">
-          <TodoCompleted />
-        </section>
-      </section>
+        )}
+      </TodoContext.Consumer>
     </div>
   )
 }
--- src/components/TodoCompleted.js
+++ src/components/TodoCompleted.js
@@ -1,13 +1,17 @@
 import ProgressBar from "react-customizable-progressbar";
 import "../css/TodoCompleted.css";
 
-function TodoCompleted() {
+function TodoCompleted({ total, completed }) {
+  const getPercent = (completNumber, totalNumber) => {
+    const percent = (completNumber / totalNumber) * 100
+    return isNaN(percent) ? 0 : percent;
+  };
   return (
     <div className="TodoCompleted-container">
       <h2>Tareas Completadas</h2>
       <div className="graphic-container">
         <ProgressBar
-          progress={50}
+          progress={getPercent(completed, total)}
           strokeColor={"#85e486"}
           trackStrokeWidth={10}
           strokeWidth={10}
@@ -19,7 +23,7 @@ function TodoCompleted() {
           initialAnimationDelay={2000}
           className={"graphic"}
         />
-        <p>50%</p>
+        <p>{`${getPercent(completed, total)}%`}</p>
       </div>
     </div>
   );
